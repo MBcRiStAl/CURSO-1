74d29ffa935bfe6f512ba23e075dc11a9bbddca8
MBcRiStAl <cristal.martinez@escola.pr.gov.br>
2025-05-29 09:29:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Em que ano começou a Primeira Guerra Mundial?',
+    'Em 1914.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quem é o Pai da Matemática?',
+    'Arquimedes.'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como é questão em espanhol?',
+    'questões {plural, cuestiones}.'
+)
+
+criaCartao(
+    'LÍngua Portuguesa',
+    'O que é acentuação gráfica?',
+    'É o conjunto de regras que determina o uso de acentos ortográficos na escrita da língua portuguesa.'
+)
